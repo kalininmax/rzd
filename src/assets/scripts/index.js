8445f83e2411a6b209507a786218f3326692d37d
Maksim Kalinin <kalinin.m.a@bk.ru>
2023-10-06 12:19:29 +0300

    Initial commit

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,29 @@
+// import { gsap } from 'gsap';
+
+// import { ScrollToPlugin } from 'gsap/ScrollToPlugin.js';
+// gsap.registerPlugin(ScrollToPlugin);
+
+// window.gsap = gsap;
+
+// gsap.defaults({
+// 	overwrite: 'auto',
+// });
+
+const HTML_CLASSLIST = document.documentElement.classList;
+
+class ProjectApp {
+	constructor() {
+		this.env = require('./utils/env').default;
+		this.utils = require('./utils/utils').default;
+		this.classes = {};
+		this.modules = {};
+		this.components = {};
+		this.helpers = {};
+
+		window.addEventListener('load', () => {
+			HTML_CLASSLIST.remove('_loading');
+		});
+	}
+}
+
+window.ProjectApp = new ProjectApp();
